99f06b23d9269cfdffdf21c5e4dc9b800cc12a51
Gajjar, Kaushal (AU) <Kaushal.Gajjar@au.kwm.com>
2017-07-24 15:18:44 +1000

    initial commit

---
+++ src/app/search-service.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class SearchServiceService {
+
+  constructor() { }
+  public getSearchResult(query: string) {
+    // http Get
+    
+  }
+}
